78982066529ba2a7ceeb7e84b3bba0c42627a376
Sneha-P2905 <snehap2919@gmail.com>
2025-02-10 09:52:21 +0530

    Time: 31 ms (92.79%), Space: 17.7 MB (38.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -3,7 +3,7 @@ class Solution:
         prev1=0
         prev=1
         if n==0:
-            return prev1
+            return 0
         for i in range(2,n+1):
             curr=prev+prev1
             prev1=prev
