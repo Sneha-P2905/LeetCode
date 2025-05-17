01ed5f2697904467a58bb45c047019d0a7dc68d0
Sneha-P2905 <snehap2919@gmail.com>
2025-05-17 20:47:49 +0530

    Time: 7 ms (81.3%), Space: 18.4 MB (40.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,6 +5,22 @@ class Solution:
         for i in arr:
             str+=i
         str=str.lower()
-        return str==str[::-1]
+        n=len(str)
+        if n==0:
+            return n==0
+        if n==1:
+            return n==1
+        i=0
+        j=n-1
+        flag=0
+        while i<j:
+            if str[i]==str[j]:
+                flag=1
+            else:
+                flag=0
+                break
+            i+=1
+            j-=1
+        return flag==1
 
         
