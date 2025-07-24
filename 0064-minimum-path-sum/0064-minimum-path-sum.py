2aa4c254ab404b8616bbe32facce33893ad31e84
Sneha-P2905 <snehap2919@gmail.com>
2025-07-24 16:12:05 +0530

    Time: 41 ms (17.13%), Space: 20.6 MB (24.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,15 +3,16 @@ class Solution:
         m=len(grid)
         n=len(grid[0])
         dp=[[-1 for i in range(n+1)] for i in range(m+1)]
-        return self.path(0,0,m,n,dp,grid)
-    def path(self,ind1,ind2,m,n,dp,grid):
+        return self.path(0,0,m,n,grid,dp)
+    def path(self,ind1,ind2,m,n,grid,dp):
         if ind1>=m or ind2>=n:
             return float("inf")
         if ind1==m-1 and ind2==n-1:
             return grid[ind1][ind2]
         if dp[ind1][ind2]!=-1:
             return dp[ind1][ind2]
-        right=grid[ind1][ind2]+self.path(ind1,ind2+1,m,n,dp,grid)
-        bottom=grid[ind1][ind2]+self.path(ind1+1,ind2,m,n,dp,grid)
+        right=grid[ind1][ind2]+self.path(ind1,ind2+1,m,n,grid,dp)
+        bottom=grid[ind1][ind2]+self.path(ind1+1,ind2,m,n,grid,dp)
         dp[ind1][ind2]=min(right,bottom)
         return dp[ind1][ind2]
+        
