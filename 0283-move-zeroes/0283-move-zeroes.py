93bdfb6e75cf42d41a12bcf8cd5aff0a73d35f38
Sneha-P2905 <snehap2919@gmail.com>
2025-07-31 12:59:04 +0530

    Time: 2 ms (83.97%), Space: 19 MB (9.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,15 +5,11 @@ class Solution:
         """
         n=len(nums)
         i=j=0
-        while i<n-1 and j<n-1:
+        while i<n and j<n:
             if nums[i]!=0:
+                nums[j],nums[i]=nums[i],nums[j]
                 i+=1
                 j+=1
             else:
-                if nums[i+1]!=0:
-                    nums[j],nums[i+1]=nums[i+1],nums[j]
-                    i+=1
-                    j+=1
-                else:
-                    i+=1
-        
+                i+=1
+        return nums
